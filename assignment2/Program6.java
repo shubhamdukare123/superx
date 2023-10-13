a83a95bee91a8d368001b4bb0a830199a796d76d
shubhamdukare123 <shubhamsdukare@gmail.com>
2023-10-13 23:11:54 +0530

    assignment2

--- assignment2/Program6.java
+++ assignment2/Program6.java
@@ -4,16 +4,14 @@ class PrimeNo{
 		Scanner sc = new Scanner(System.in);
 		int start = sc.nextInt();
 		int end = sc.nextInt();
-		boolean flag = true;
 		for(int i=start; i<=end; i++){
+			int count=0;
 			for(int j=2; j<i; j++){
 				if(i%j==0){
-					System.out.println(i);
-					flag=false;
-					break;
+					count++;	
 				}
 			}
-			if(flag==true)
+			if(count==0 && i!=1)
 				System.out.println("Prime No :" + i);
 		}
 	}
