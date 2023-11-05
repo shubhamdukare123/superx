ecead4aa3d146dcb91db85aefaa17c0072d3436c
shubhamdukare123 <shubhamsdukare@gmail.com>
2023-11-05 19:04:28 +0530

    leetcode

--- leetCode/6/Program1.java
+++ leetCode/6/Program1.java
@@ -1,44 +1,58 @@
-import java.util.*;
-class UpLowCase{
-	//i love core2web
+class Solution {
+    public String capitalizeTitle(String title) {
+        char[] chStr = title.toCharArray();
+        for(int i=0; i<chStr.length; i++){
+            int count = 0;
+            for(int j=i; j<chStr.length; j++){
+                count++;
+                if(chStr[j] == ' '){
+                    if(count<=3){
+                        for(int k=i+1; k<=j; k++){
+                             if(chStr[i]>=65 && chStr[i]<=90){
+                                chStr[i] = (char)(chStr[i] + 32);
+                             }
+                             if(chStr[k]>=65 && chStr[k]<=90){
+                                 chStr[k] = (char)(chStr[k]+32);
+                             }
+                        }
+                    }else{
+                        for(int k=i+1; k<j; k++){
+                            if(chStr[i]>=97 && chStr[i]<=122){
+                                chStr[i] = (char)(chStr[i] - 32);
+                            }
+                            if(chStr[k]>=65 && chStr[k]<=90){
+                                chStr[k] = (char)(chStr[k]+32);
+                            }
+                        }
+                    }
+                
+                    i=j;
+                    break;  
+                }
+                else if(j == chStr.length-1){
+                    if(count<=2){
+                            for(int k=i; k<=j; k++){
+                                if(chStr[k] >=65 && chStr[k] <= 90){
+                                    chStr[k] = (char)(chStr[k]+32);
+                                }
+                            }
 
-	static String capitalizeStr(String str){
-		char ch[] = str.toCharArray();
-		for(int i=0; i<ch.length; i++){
-			int count=0;
-			for(int j=i+1; j<ch.length ;j++){
-				count++;
-				if(ch[j] == ' '){
-					for(int k=i+1; k<=j; k++){
-						if(count <=2){
-							if(ch[i] >=65 && ch[i] <= 90){
-								ch[i] = (char)(ch[i] + 32);
-							}
-							if(ch[k] >=65 && ch[k] <= 90)
-								ch[k] = (char)(ch[k]+32);	
-						}
-						else{
-							if(ch[i] >=97 && ch[i] <=122){
-								System.out.println("cap");
-								ch[i] = (char)(ch[i]-32);
-							}
-							if(ch[k] >=65 && ch[k] <= 90){
-								ch[k] = (char)(ch[k] + 32);
-							}
-						}
-					}
-					i = j+1;
-					break;
-				}
-			}
-		}
-		str = str.valueOf(ch);
-		return str;
-	}
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		System.out.println(capitalizeStr(str));
-	
-	}
+                    }else{
+                        for(int k=i+1; k<=j; k++){
+                            if(chStr[i]>=97 && chStr[i]<=122){
+                                chStr[i] = (char)(chStr[i] - 32);
+                            }
+                            if(chStr[k]>=65 && chStr[k]<=90){
+                                chStr[k] = (char)(chStr[k]+32);
+                            }
+                        }    
+                    }
+                 i=j+1;   
+                }                     
+                
+            }
+        }
+        title = String.valueOf(chStr);
+        return title;
+    }
 }
